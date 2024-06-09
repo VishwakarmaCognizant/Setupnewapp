2779bcb3cb0274dc6be00dbfae4b83a609a942ca
Mahesh <maheshv04kumar@gmail.com>
2024-06-09 17:13:38 +0530

    add formmodule  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactUsComponent } from './Contact/contact-us/contact-us.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
